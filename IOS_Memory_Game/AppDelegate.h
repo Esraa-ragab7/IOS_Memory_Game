469a03ff53d555f312c3321747dc6d7004652525
Esraa Ragab <esraa.ragab@Tagipedia.com>
2017-11-29 11:27:29 +0200

    Initial Commit

---
+++ IOS_Memory_Game/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS_Memory_Game
+//
+//  Created by Tagipedia on 11/29/17.
+//  Copyright © 2017 Tagipedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS_Memory_Game/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS_Memory_Game
+//
+//  Created by Tagipedia on 11/29/17.
+//  Copyright © 2017 Tagipedia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
